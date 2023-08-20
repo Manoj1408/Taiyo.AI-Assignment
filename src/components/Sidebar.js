559b09e616c9a6d3bb7cf64ed5962206346dc700
Manoj1408 <manojkumarreddy@alumni.iitm.ac.in>
2023-08-20 11:02:11 +0530

    Deployed website

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
 
 export default function Sidebar() {
-    return (
-      <>
-        <div className="sider-comp">
-            <NavLink to="/TaiyoAi-Project/">Contact</NavLink>
-            <NavLink to="/TaiyoAi-Project/map">Charts and Maps</NavLink>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="sider-comp">
+        <NavLink to="/Taiyo.AI-Assignment/">Contact</NavLink>
+        <NavLink to="/Taiyo.AI-Assignment/map">Charts and Maps</NavLink>
+      </div>
+    </>
+  );
 }
