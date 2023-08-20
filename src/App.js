feb4e630c50913b0d152d6e9e1642099365fe9e0
Manoj1408 <manojkumarreddy@alumni.iitm.ac.in>
2023-08-20 10:52:57 +0530

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Sidebar from './components/Sidebar';
-import Contacts from './components/Contacts';
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Contacts from "./components/Contacts";
 import { Routes, Route } from "react-router-dom";
-import Map from './components/Map'
-import Header from './components/Header';
+import Map from "./components/Map";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
           <Sidebar />
 
           <Routes>
-            <Route path="/TaiyoAi-Project/" element={<Contacts />}></Route>
-            <Route path="/TaiyoAi-Project/map" element={<Map />}></Route>
+            <Route path="/Taiyo.AI-Assignment/" element={<Contacts />}></Route>
+            <Route path="/Taiyo.AI-Assignment/map" element={<Map />}></Route>
           </Routes>
         </div>
       </div>
